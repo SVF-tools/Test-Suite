ac6ab8e6b06cf730c232048c49964eac650e11e5
jumormt <jumormt@163.com>
2021-09-06 13:25:32 +0800

    add debug info

--- generate_bc.sh
+++ generate_bc.sh
@@ -116,7 +116,7 @@ do
     ########
     # created a .ll, let's make it .bc, as the filename suggests.
     ########
-    $compiler -Wno-everything -S -emit-llvm -fno-discard-value-names -I"$root" "$c_f" -o "$bc_f"
+    $compiler -Wno-everything -S -emit-llvm -fno-discard-value-names -g -I"$root" "$c_f" -o "$bc_f"
     llvm-as "$bc_f" -o "$bc_f"
     opt -mem2reg "$bc_f" -o "$bc_f"
   done
