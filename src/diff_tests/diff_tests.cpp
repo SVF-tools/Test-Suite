cd2bce01fe64f85a2be789808b41e8a70f5e9dd9
lukekoko <github@lukekoko.net>
2021-11-14 23:09:19 +1100

    add more statistics to compare for SVFG

--- src/diff_tests/diff_tests.cpp
+++ src/diff_tests/diff_tests.cpp
@@ -5,71 +5,85 @@
 #include <string>
 #include <sstream>
 #include <array>
+#include <vector>
+#include <algorithm>
 
-using namespace std;
-
-string exec_command(const char* cmd) {
+std::string exec_command(const char* cmd) 
+{
     std::array<char, 128> buffer;
     std::string result;
     std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-    if (!pipe) {
+    if (!pipe) 
+    {
         throw std::runtime_error("popen() failed!");
     }
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) 
+    {
         result += buffer.data();
     }
     return result;
 }
 
-string extractData(const string str, string type) {
+std::string extractData(const std::string str, std::string type) 
+{
+    std::stringstream iss(str);
+    std::string line;
+    std::stringstream output;
+    std::vector<std::string> search;
 
-    stringstream iss(str);
-    string line;
-    std::stringstream read_write_svfg_ss;
-    while(getline(iss, line)) {
-        string temp = line; 
-        if(type == "read_write_svfg"){
-            if (temp.find("AvgTopLvlPtsSize") != std::string::npos || temp.find("TotalNode") != std::string::npos || temp.find("TotalEdge") != std::string::npos) {
-            read_write_svfg_ss << temp << ","; 
-            }
-        } else { 
-            if (temp.find("AvgTopLvlPtsSize") == 0) {
-            return temp;
+    if(type == "read_write_svfg")
+    {
+        search = {"AvgTopLvlPtsSize", "TotalNode", "TotalEdge", "AvgIndOutDeg", "AvgIndInDeg", "AvgOutDegree", "AvgInDegree", "AvgWeight"};
+    } 
+    else 
+    { 
+        search = {"AvgTopLvlPtsSize"};
+    }
+    while(getline(iss, line)) 
+    {
+        for (std::vector<std::string>::iterator i = search.begin(); i != search.end(); ++i) 
+        {   
+            if (line.find(*i) != std::string::npos) 
+            {
+                output << line << ","; 
             }
         }
     }
-
-    return type == "read_write_svfg" ? read_write_svfg_ss.str() : line;
+    return output.str();
 }
 
 int main(int argc, char *argv[])
 {
 
-    string cmd1 = argv[1];
-    string cmd2 = argv[2];
-    string folder = argv[3];
-    string result, result2;
+    std::string cmd1 = argv[1];
+    std::string cmd2 = argv[2];
+    std::string folder = argv[3];
+    std::string result, result2;
     result = exec_command(("./" + cmd1 + " " +  folder).c_str());
     if (argc == 5)
     {
-        string folder2 = argv[4];
+        std::string folder2 = argv[4];
         result2 = exec_command(("./" + cmd2 + " " +  folder2).c_str());
     }
-    else {
+    else 
+    {
         result2 = exec_command(("./" + cmd2 + " " +  folder).c_str());
     }
 
-    string data1;
-    string data2; 
+    std::string data1;
+    std::string data2; 
 
     std::size_t read_write_svfg = cmd1.find("write-svfg");
-    if (read_write_svfg!=std::string::npos){
+    if (read_write_svfg!=std::string::npos)
+    {
         data1 = extractData(result, "read_write_svfg");
         data2 = extractData(result2, "read_write_svfg");
-    } else { 
+    } 
+    else 
+    { 
         data1 = extractData(result, "ander_nander,read_write_ander");
         data2 = extractData(result2, "ander_nander,read_write_ander");
     }
 
-    std::cout << data1.compare(data2) << endl;
+    std::cout << data1.compare(data2) << std::endl;
 }
