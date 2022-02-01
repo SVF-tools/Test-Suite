a4b48602e1d845a96c2e2d57ea5902ba47126279
Yulei Sui <rockysui@gmail.com>
2022-02-02 10:36:54 +1100

    add comment for spec-gap.c

--- src/basic_c_tests/spec-gap.c
+++ src/basic_c_tests/spec-gap.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include "aliascheck.h"
 
+/// Note that `SyGetmem` is summarized lib function (with side-effect ExtAPI::EFT_ALLOC) in ExtAPI.cpp, we will use SVF's summmary instead of this user defined function
 char *  SyGetmem ( long size ){}
 
 typedef struct TypHeader {
