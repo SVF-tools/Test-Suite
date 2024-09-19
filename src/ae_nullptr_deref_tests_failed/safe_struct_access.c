4fe6bf3d99288bee775a720227f8af388d6dd4ab
Ethan Lin <eeeeethanl@gmail.com>
2024-09-19 23:11:03 +1000

    added comment for fail reason

--- src/ae_nullptr_deref_tests_failed/safe_struct_access.c
+++ src/ae_nullptr_deref_tests_failed/safe_struct_access.c
@@ -14,6 +14,11 @@ int main() {
     dataPtr->character = 'a';
 
     SAFE_LOAD(dataPtr);
+    /*
+        This failed because dataPtr's addr (e.g 0x...6a) is not in _addrToAbsVal
+        store on dataPtr->number => gep(dataPtr, 0) => generate a new addr (e.g 0x...ee) to store in _addrToAbsVal
+        Therefore, only (0x...ee) is in _addrToAbsVal but not (0x...6a)
+    */
 
     return 0;
 }
