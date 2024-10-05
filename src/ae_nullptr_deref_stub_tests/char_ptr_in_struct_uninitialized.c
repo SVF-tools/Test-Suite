851728c8aa1aacd326dafad22079426fc457fde4
Ethan Lin <ethan@Ethans-MacBook-Air.local>
2024-10-05 21:29:14 +1000

    added struct testcase

---
+++ src/ae_nullptr_deref_stub_tests/char_ptr_in_struct_uninitialized.c
@@ -0,0 +1,19 @@
+//
+// Created by Ethan Lin on 30/9/2024.
+//
+
+#include <stdio.h>
+
+extern void UNSAFE_LOAD(void *ptr);
+
+struct S {
+    char *ptr;
+};
+
+int main() {
+    struct S myStruct;
+
+    UNSAFE_LOAD(myStruct.ptr);
+
+    return 0;
+}
